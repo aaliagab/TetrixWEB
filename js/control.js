804bab86417fdf6389a9dbbd014d48478f115b62
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-06-25 18:36:02 -0400

    move cuadrado

--- js/control.js
+++ js/control.js
@@ -1,21 +1,3 @@
-document.onkeydown = function(e) {
-    if(e.key=='ArrowDown')
-    {
-        //alert('Down');
-        down();
-    }else if(e.key=='ArrowRight')
-    {
-        alert('Right');
-    }else if(e.key=='ArrowLeft')
-    {
-        alert('Left');
-    }else if(e.key=='ArrowUp')
-    {
-        alert('Up');
-    }
-    //alert(e.key+e.keyCode); // shows k75
-};
-
 
 let tbody = document.getElementById('bodytable');
 let trcount = 0;
@@ -96,56 +78,95 @@ function colocarFiguraProxima(){
     }
 }
 
+export let figura_actual = '';
 
-
-function colocarFiguras(){
+export function colocarFiguras(){
     if(figs[position][1][0].className == 'cagua'){
         //dibuja linea color agua
+        figura_actual = 'linea';
         document.getElementById('img-'+0+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+2+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+3+'-'+4).className = figs[position][1][0].className;
     }else if(figs[position][1][0].className == 'camarillo'){
         //dibuja T color amarillo
+        figura_actual = 'T';
         document.getElementById('img-'+0+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+2+'-'+5).className = figs[position][1][0].className;
     }else if(figs[position][1][0].className == 'cazul'){
         //dibuja Linvertida color azul
+        figura_actual = 'Linvertida';
         document.getElementById('img-'+0+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+6).className = figs[position][1][0].className;
     }else if(figs[position][1][0].className == 'cverde'){
         //dibuja L color verde
+        figura_actual = 'L';
         document.getElementById('img-'+0+'-'+6).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+6).className = figs[position][1][0].className;
     }else if(figs[position][1][0].className == 'cmorado'){
         //dibuja S color morado
+        figura_actual = 'S';
         document.getElementById('img-'+0+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+0+'-'+6).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+5).className = figs[position][1][0].className;
     }else if(figs[position][1][0].className == 'cnaranja'){
         //dibuja Sinvertida color naranja
+        figura_actual = 'Sinvertida';
         document.getElementById('img-'+0+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+2+'-'+4).className = figs[position][1][0].className;
     }else{
         //dibuja cuadrado color rojo
+        figura_actual = 'cuadrado';
         document.getElementById('img-'+0+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+0+'-'+5).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+4).className = figs[position][1][0].className;
         document.getElementById('img-'+1+'-'+5).className = figs[position][1][0].className;
     }
     colocarFiguraProxima();
+    position++;
 }
 
-colocarFiguras();
+import * as move from "./movimientos.js";
+export let pausa = false;
+export let timedown = 1000; //tiempo de caida
+document.onkeydown = function(e) {
+    if(e.key=='ArrowDown' && !pausa)
+    {
+        move.moveDown(figura_actual);
+    }else if(e.key=='ArrowRight' && !pausa)
+    {
+        move.moveRight(figura_actual);
+    }else if(e.key=='ArrowLeft' && !pausa)
+    {
+        move.moveLeft(figura_actual);
+    }else if(e.key=='ArrowUp' && !pausa)
+    {
+        move.moveRotar(figura_actual);
+    }else if(e.key=='P')
+    {
+        console.log(e.key);
+       pausa = !pausa;
+    }
+    //alert(e.key+e.keyCode); // shows k75
+};
+
+
+
+function gameStart(){
+    colocarFiguras(); 
+    //move.caidaConstante();
+}
+
+gameStart();
 
 //document.getElementById('img-'+5+'-'+5).className = figs[0][1][0].className;
 //document.getElementById('img-'+5+'-'+5).className = 'crojo';
---
+++ js/movimientos.js
@@ -0,0 +1,493 @@
+let fila_inicial = 0;
+let columna_inicial = 4;
+let rotacion = 0;//puede ser 0,1,2,3
+
+import * as control from "./control.js";
+
+export function caidaConstante(){    
+    while(!control.pausa){
+        let myVar = setTimeout(moveDown(control.figura_actual),control.timedown);
+        clearTimeout(myVar);
+    }
+}
+
+//caidaConstante();
+
+function iniciarNuevaFigura(){
+    fila_inicial = 0;
+    columna_inicial = 4;
+    rotacion = 0;    
+    borrarFilas();
+    control.colocarFiguras();    
+}
+
+function isFilaCompleta(fila){
+    let columna = 0;
+    while(columna<12){
+        //console.log(fila+" - "+columna);
+        if(document.getElementById('img-'+(fila)+'-'+columna).className=='cnone')
+            return false;
+        columna++;
+    }
+    return true;
+}
+
+function copiarFila(origen,destino){
+    for(let i = 0; i<12;i++){
+        if(origen<16 && destino<16)
+        document.getElementById('img-'+(destino)+'-'+i).className=
+        document.getElementById('img-'+(origen)+'-'+i).className;
+    }
+}
+
+function limpiarFila(fila){
+    for(let i = 0; i<12;i++){
+        document.getElementById('img-'+(fila)+'-'+i).className ='cnone';
+    }
+}
+
+function borrarFilas(){
+    let inicio = -1;
+    let fin = -1;
+    let fila = 0;
+    while(fila<16 && !isFilaCompleta(fila)){
+        fila++;
+    }
+    if(fila>0 && fila<16){
+        inicio = fila;
+        fin = fila;
+    }
+    fila++;
+    while(fila<16 && isFilaCompleta(fila)){
+        fila++;
+    }    
+    fin = fila-1;
+    //let destino = inicio;//15
+    /*for(let i = 2*inicio-fin-1;i<inicio;i++){//i=14-15
+        if(i<0)
+            limpiarFila(destino);
+        else{
+            copiarFila(i,destino++);            
+        }
+    }*/
+    //inicio 14 fin 15 eliminar 2
+    let i = inicio-1;
+    console.log('i ' + i);
+    console.log('inicio ' + inicio);
+    console.log('fin ' + fin);
+    while(fin>=0 && inicio>=0){
+        console.log('while');
+        if(i<0){
+            console.log('limpiar');
+            limpiarFila(fin--);
+            i--;
+        }
+        else{
+            console.log('copiar de ' + i + ' '+fin);
+            copiarFila(i--,fin--);            
+        }
+    }
+    
+}
+
+function isVacio(fila,columna){
+    return document.getElementById('img-'+fila+'-'+columna).className=='cnone';
+}
+
+function moveDownLinea(){
+    if(rotacion == 0){
+        if(fila_inicial<12 &&
+            document.getElementById('img-'+(fila_inicial+4)+'-'+columna_inicial).className=='cnone'){
+                document.getElementById('img-'+(fila_inicial)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className='cagua';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className='cagua';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+columna_inicial).className='cagua';
+                document.getElementById('img-'+(fila_inicial+4)+'-'+columna_inicial).className='cagua';
+                fila_inicial++;
+                if(fila_inicial==12){
+                    iniciarNuevaFigura();
+                }
+            }
+        else if(document.getElementById('img-'+(fila_inicial+4)+'-'+columna_inicial).className!='cnone'){                    
+            iniciarNuevaFigura();                    
+            }
+    }else if(rotacion == 1){
+        if(fila_inicial<15 &&
+            document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className=='cnone' &&
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+1)).className=='cnone' &&
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+2)).className=='cnone' &&
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+3)).className=='cnone'){
+                document.getElementById('img-'+(fila_inicial)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+3)).className='cnone';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+1)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+2)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+3)).className='cagua';
+                fila_inicial++;
+                if(fila_inicial==15){
+                    iniciarNuevaFigura();
+                }
+            }
+        else if(document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className!='cnone' ||
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+1)).className!='cnone' ||
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+2)).className!='cnone' ||
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+3)).className!='cnone'){                    
+                iniciarNuevaFigura();                    
+            }
+    }
+}
+
+function moveDownCuadrado(){
+    if(fila_inicial<14 &&
+        document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className=='cnone' &&
+        document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial+1)).className=='cnone'){
+            document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial)).className='cnone';
+            document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cnone';
+            document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial)).className='crojo';
+            document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial+1)).className='crojo';
+            fila_inicial++;
+            if(fila_inicial==14){
+                iniciarNuevaFigura();
+            }
+        }
+    else if(document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className!='cnone' ||
+            document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial+1)).className!='cnone'){                    
+                iniciarNuevaFigura();                    
+        }
+}
+
+function moveLeftCuadrado(){
+    if(columna_inicial>0 &&
+        document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className=='cnone' &&
+        document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial-1)).className=='cnone'){
+            document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cnone';
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+1)).className='cnone';
+            document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='crojo';
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial-1)).className='crojo';
+            columna_inicial--;
+            if(fila_inicial==14 && columna_inicial==0){
+                iniciarNuevaFigura();
+            }
+        }
+}
+
+function moveRightCuadrado(){
+    console.log('RIGHT');
+    if(columna_inicial<10 &&
+        document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className=='cnone' &&
+        document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+2)).className=='cnone'){
+            document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial)).className='cnone';
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial)).className='cnone';
+            document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className='crojo';
+            document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial+2)).className='crojo';
+            columna_inicial++;
+            if(fila_inicial==14 && columna_inicial==10){
+                iniciarNuevaFigura();
+            }
+        }
+}
+
+
+export function moveDown(figura_actual){
+    if(figura_actual == 'linea'){
+        moveDownLinea();
+    }else if(figura_actual == 'T'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'Linvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'L'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'S'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else if(figura_actual == 'Sinvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else{//figura_actual == 'cuadrado'
+        moveDownCuadrado();
+    }
+}
+
+export function moveLeft(figura_actual){
+    if(figura_actual == 'linea'){
+        
+    }else if(figura_actual == 'T'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'Linvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'L'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'S'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else if(figura_actual == 'Sinvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else{//figura_actual == 'cuadrado'
+        moveLeftCuadrado();
+    }
+}
+
+export function moveRight(figura_actual){
+    if(figura_actual == 'linea'){
+        
+    }else if(figura_actual == 'T'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'Linvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'L'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'S'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else if(figura_actual == 'Sinvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else{//figura_actual == 'cuadrado'
+        moveRightCuadrado();
+    }
+}
+
+function rotarLinea(){
+    if(rotacion == 0){
+        if(columna_inicial<=8 &&
+            isVacio(fila_inicial,columna_inicial+1) && 
+            isVacio(fila_inicial,columna_inicial+2) && 
+            isVacio(fila_inicial,columna_inicial+3)){
+                document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+3)).className='cagua';
+                rotacion = 1;
+            }
+        else if(columna_inicial==9 &&
+            isVacio(fila_inicial,columna_inicial-1) && 
+            isVacio(fila_inicial,columna_inicial+1) && 
+            isVacio(fila_inicial,columna_inicial+2)){
+                document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className='cagua';
+                rotacion = 1;
+            }
+        else if(columna_inicial==10 &&
+            isVacio(fila_inicial,columna_inicial-1) && 
+            isVacio(fila_inicial,columna_inicial-2) && 
+            isVacio(fila_inicial,columna_inicial+1)){
+                document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-2)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cagua';
+                rotacion = 1;
+            }
+        else if(columna_inicial==11 &&
+            isVacio(fila_inicial,columna_inicial-1) && 
+            isVacio(fila_inicial,columna_inicial-2) && 
+            isVacio(fila_inicial,columna_inicial-3)){
+                document.getElementById('img-'+(fila_inicial+1)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+columna_inicial).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-2)).className='cagua';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-3)).className='cagua';
+                rotacion = 1;
+            }
+    }else if(rotacion == 1){
+        if(columna_inicial<=8 &&
+            isVacio(fila_inicial+1,columna_inicial) && 
+            isVacio(fila_inicial+2,columna_inicial) && 
+            isVacio(fila_inicial+3,columna_inicial)){
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+3)).className='cnone';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+(columna_inicial)).className='cagua';
+                rotacion = 0;
+            }
+        else if(columna_inicial==9 &&
+            isVacio(fila_inicial+1,columna_inicial) && 
+            isVacio(fila_inicial+2,columna_inicial) && 
+            isVacio(fila_inicial+3,columna_inicial)){
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+2)).className='cnone';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+(columna_inicial)).className='cagua';
+                rotacion = 0;
+            }
+        else if(columna_inicial==10 &&
+            isVacio(fila_inicial+1,columna_inicial) && 
+            isVacio(fila_inicial+2,columna_inicial) && 
+            isVacio(fila_inicial+3,columna_inicial)){
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-2)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial+1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+(columna_inicial)).className='cagua';
+                rotacion = 0;
+            }
+        else if(columna_inicial==11 &&
+            isVacio(fila_inicial+1,columna_inicial) && 
+            isVacio(fila_inicial+2,columna_inicial) && 
+            isVacio(fila_inicial+3,columna_inicial)){
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-1)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-2)).className='cnone';
+                document.getElementById('img-'+(fila_inicial)+'-'+(columna_inicial-3)).className='cnone';
+                document.getElementById('img-'+(fila_inicial+1)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+2)+'-'+(columna_inicial)).className='cagua';
+                document.getElementById('img-'+(fila_inicial+3)+'-'+(columna_inicial)).className='cagua';
+                rotacion = 0;
+            }
+    }
+}
+
+export function moveRotar(figura_actual){
+    if(figura_actual == 'linea'){
+        rotarLinea();        
+    }else if(figura_actual == 'T'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'Linvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'L'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }else if(rotacion == 2){
+
+        }else{//rotacion 3
+
+        }
+    }else if(figura_actual == 'S'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else if(figura_actual == 'Sinvertida'){
+        if(rotacion == 0){
+
+        }else if(rotacion == 1){
+
+        }
+    }else{//figura_actual == 'cuadrado'
+        
+    }
+}
